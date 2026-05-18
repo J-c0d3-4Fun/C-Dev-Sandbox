79c714618b1cfd6d8112279395a7d52eb45a3286
J-c0d3-4Fun <jabbrown001@gmail.com>
2026-05-18 07:48:14 -0400

    Adding new c code

---
+++ learning/c-programming/the-c-ansi-programming-language/exercise-1-1.c
@@ -0,0 +1,10 @@
+// Run "hello, world" program on your system with leaving out parts 
+// of the program, to see what error messages you get
+#include <stdio.h>
+
+
+int main() {
+    printf("hello, ");
+    printf("world!");
+    printf("\n");
+}
---
+++ learning/c-programming/the-c-ansi-programming-language/exercise-1-4.c
@@ -0,0 +1,31 @@
+// Write a program to print the corresponding Celsius to Fahrenheit table
+
+
+/*
+Research:
+(32°F − 32) × 5/9 = 0°C
+
+*/ 
+
+#include <stdio.h>
+
+int main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celsius = lower;
+
+    printf("Celsius-Farenheit Table\n");
+    printf("-----------------------\n");
+
+    while (celsius <= upper) {
+        fahr = (9.0/5.0) * celsius + 32;
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+
+}
